d784d3bbc7518fdc929080bb007efd23d23fad03
Jake Sharpe <your_email@example.com>
2020-03-02 13:42:18 +0000

    Fully responsive

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const img1 = document.querySelector(".img-1");
+  const img1 = document.querySelector(".img-1");
     const img2 = document.querySelector(".img-2");
     const img3 = document.querySelector(".img-3");
     const img4 = document.querySelector(".img-4");
